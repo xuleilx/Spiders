ceb6da6b1969f6681b4818a4155a4ae4eb1454f7
徐磊 <xuleilx@gmail.com>
2019-02-13 15:05:00 +0800

    基于通用爬虫CrawlSpider，编写链接过滤器

--- pic/pic/spiders/xh.py
+++ pic/pic/spiders/xh.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 from pic.items import PicItem
 
-
-class XhSpider(scrapy.Spider):
+class XhSpider(CrawlSpider):
     name = 'xh'
     # 限制爬虫只在该域名下爬
     allowed_domains = ['xiaohuar.com']
     start_urls = ['http://www.xiaohuar.com/list-1-1.html']
+    rules = (
+        # Extract links matching 'category.php' (but not matching 'subsection.php')
+        # and follow links from them (since no callback means follow=True by default).
+        # Rule(LinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
 
-    def parse(self, response):
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        # Rule(LinkExtractor(allow=('item\.php', )), callback='parse_item'),
+        # callback不要使用默认的parse
+        Rule(LinkExtractor(allow=('list-1-(\d)+\.html', )), callback='parse_item', follow=True),
+
+    )
+    def parse_item(self, response):
         allPics = response.xpath('//div[@class="img"]/a')
         for pic in allPics:
             item = PicItem()
@@ -19,10 +29,7 @@ class XhSpider(scrapy.Spider):
             print item['addr']
             # 返回爬取到的数据
             yield item
-
-        # 获取所有的地址链接
-        for url in response.xpath("//a/@href"):
-            yield response.follow(url,callback=self.parse)
-
-
-
+        #
+        # # 获取所有的地址链接
+        # for url in response.xpath("//a/@href"):
+        #     yield response.follow(url,callback=self.parse)
